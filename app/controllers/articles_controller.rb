8f7aa60a9ba1e7926c6ed116b7773586210f1694
mostafa abdelsalam <mostafa.abdelsalam96@gmail.com>
2020-12-06 18:41:17 +0100

    Added articles views and routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    
+    before_action :authenticate_user!
+
     def index
         @articles = Article.all
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'articles#index'
   devise_for :users
+
+  resources :users do
+    resources :articles
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
